682a247baab22a2273947b59b1e0ed71ad23927b
S <46511231+pawnJj@users.noreply.github.com>
2025-09-23 23:40:54 +0800

    s

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  compiler: isProd
+    ? {
+      // 在生产构建时移除 console 调用（保留 error/warn）
+      removeConsole: { exclude: ['error', 'warn'] },
+    }
+    : undefined,
 };
 
 module.exports = nextConfig;
