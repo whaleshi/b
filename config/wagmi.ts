bae6974191ebfbbd25296a84569add68a7082a3a
S <46511231+pawnJj@users.noreply.github.com>
2025-09-23 22:02:46 +0800

    last

--- config/wagmi.ts
+++ config/wagmi.ts
@@ -3,20 +3,20 @@ import { injected } from "wagmi/connectors";
 import { xLayer, morphHolesky } from "wagmi/chains";
 import { defineChain } from "viem";
 
-const customNetwork = defineChain({
-    id: 31337,
-    name: "Custom Network",
-    nativeCurrency: {
-        decimals: 18,
-        name: "Ether",
-        symbol: "ETH",
-    },
-    rpcUrls: {
-        default: {
-            http: ["http://43.160.204.41:8545"],
-        },
-    },
-});
+// const customNetwork = defineChain({
+//     id: 31337,
+//     name: "Custom Network",
+//     nativeCurrency: {
+//         decimals: 18,
+//         name: "Ether",
+//         symbol: "ETH",
+//     },
+//     rpcUrls: {
+//         default: {
+//             http: ["http://43.160.204.41:8545"],
+//         },
+//     },
+// });
 
 export const config = createConfig({
     chains: [xLayer],
