b8ba9b384f9772b9b068658480df154a82f6252d
S <46511231+pawnJj@users.noreply.github.com>
2025-09-24 17:16:37 +0800

    s

--- config/wagmi.ts
+++ config/wagmi.ts
@@ -1,7 +1,12 @@
-import { http } from "wagmi";
+import { http, createConfig } from "wagmi";
 import { xLayer, morphHolesky } from "wagmi/chains";
 import { defineChain } from "viem";
-import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+import { connectorsForWallets } from "@rainbow-me/rainbowkit";
+import {
+    metaMaskWallet,
+    okxWallet,
+    walletConnectWallet,
+} from "@rainbow-me/rainbowkit/wallets";
 
 // const customNetwork = defineChain({
 //     id: 31337,
@@ -18,9 +23,25 @@ import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 //     },
 // });
 
-export const config = getDefaultConfig({
-    appName: 'xboz.fun',
-    projectId: 'cf29fa9397c7812afa53a3e0cdaf5764',
+const connectors = connectorsForWallets(
+    [
+        {
+            groupName: "推荐钱包",
+            wallets: [okxWallet, metaMaskWallet],
+        },
+        {
+            groupName: "其他连接方式",
+            wallets: [walletConnectWallet],
+        },
+    ],
+    {
+        appName: "xboz.fun",
+        projectId: "cf29fa9397c7812afa53a3e0cdaf5764",
+    }
+);
+
+export const config = createConfig({
+    connectors,
     chains: [xLayer],
     transports: {
         [xLayer.id]: http(),
